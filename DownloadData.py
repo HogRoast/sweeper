3cc41a1a8a9087488ba5c1676b75f742692d759b
Stuart <MonkeeFerret@gmail.com>
2018-08-28 14:57:32 +0100

    Initial repo creation

---
+++ DownloadData.py
@@ -0,0 +1,32 @@
+import urllib.request
+import csv
+
+'''
+ How often do the following url change?
+'''
+years = ['1617', '1516', '1415', '1314', '1213', '1112', '1011', '0910', '0809', '0708', '0607', '0506', '0405', '0304', '0203', '0102', '0001']
+leagues = [ 'E0', 'E1', 'E2', 'E3', 'SC0', 'SC1', 'SC2', 'SC3', 'D1', 'D2', 'I1', 'I2', 'SP1', 'SP2', 'F1', 'F2', 'N1', 'B1', 'P1', 'T1', 'G1'] 
+
+baseURL = 'http://www.football-data.co.uk/mmz4281/{}/{}.csv'
+baseOutputFilename = 'Data/{}_{}.csv'
+                
+for l in leagues:
+    for y in years:
+        resultsURL = baseURL.format(y, l)
+        print('Downloading...', resultsURL)
+        httpResp = urllib.request.urlopen(resultsURL)
+        results = str(httpResp.read())
+        csvFile = results.split('\\r\\n')
+        resultsReader = csv.reader(csvFile, delimiter=',')
+        outputFilename = baseOutputFilename.format(l, y)
+        print('Output to...', outputFilename)
+        outputFile = open(outputFilename, 'w')    
+        outputWriter = csv.writer(outputFile)
+        firstRow = True
+        for row in resultsReader:
+            if firstRow:
+                row[0] = 'Div'
+                outputWriter.writerow(row)
+                firstRow = False
+            else:
+                outputWriter.writerow(row)
---
+++ Logging.py
@@ -0,0 +1,50 @@
+from datetime import datetime
+
+class Logger:
+    TYPE     = 0x0000001
+    TIME     = 0x0000010
+    INFO     = 0x0000100
+    DEBUG    = 0x0001000
+    WARNING  = 0x0010000 
+    ERROR    = 0x0100000 
+    CRITICAL = 0x1000000
+
+    def __init__(self):
+        self.mask = self.INFO | self.WARNING | self.ERROR | self.CRITICAL
+        self.log = print
+
+    def setLogger(self, l):
+        self.log = l
+
+    def getlogger(self):
+        return self.log
+
+    def setMask(self, m):
+        self.mask = m
+
+    def toggleMask(self, m):
+        self.mask ^= m
+
+    def getMask(self):
+        return self.mask
+    
+    def _baselog(self, s):
+        ll = ''
+        if self.mask & self.TIME: ll += '{!s} : '.format(datetime.now())
+        if self.mask & self.TYPE: ll += '{:<8s} : '.format(s)
+        return ll
+        
+    def info(self, s):
+        if self.mask & self.INFO: self.log('{}{}'.format(self._baselog('INFO'), s))
+        
+    def debug(self, s):
+        if self.mask & self.DEBUG: self.log('{}{}'.format(self._baselog('DEBUG'), s))
+
+    def warning(self, s):
+        if self.mask & self.WARNING: self.log('{}{}'.format(self._baselog('WARNING'), s))
+
+    def error(self, s):
+        if self.mask & self.ERROR: self.log('{}{}'.format(self._baselog('ERROR'), s))
+
+    def critical(self, s):
+        if self.mask & self.CRITICAL: self.log('{}{}'.format(self._baselog('CRITICAL'), s))
